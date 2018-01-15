e2ef266e6f88bb411f00227f8bca0e5f98a41871
Mario Gray <mario.gray@gmail.com>
2018-01-15 10:44:38 -0800

    changes springfn demo location

--- springFnBasics/src/main/java/com/codepub/demo/riff/DemoFunction.java
+++
@@ -1,9 +0,0 @@
-package com.codepub.demo.riff;
-
-import java.util.function.Function;
-
-public class DemoFunction implements Function<String, Integer> {
-    public Integer apply(String words) {
-        return words.length();
-    }
-}
--- springFnBasics/src/main/java/com/codepub/demo/riff/SpringFunctionApp.java
+++
@@ -1,8 +0,0 @@
-package com.codepub.demo.riff;
-
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class SpringFunctionApp {
-
-}
